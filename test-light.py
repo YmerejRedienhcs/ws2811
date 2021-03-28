7d9dbfa60c006485c6625fa1d333e49514284a61
Jeremy Schneider <jschneider@esri.com>
2021-03-27 22:51:01 -0400

    playing

--- test-light.py
+++ test-light.py
@@ -4,12 +4,15 @@ import board
 import neopixel
 import time
 import random
-num_lights = 250
+num_lights = 50
 #num_lights = int(sys.argv[1])
-seg_length = 100
+seg_length = 10
 
-def t(g):
-    return (g[1], g[0], g[2])
+# transform between RGB color and GBR
+# goes both ways since it just swaps the first two of the tuple
+def t(c):
+    return (c[1], c[0], c[2])
+
 black = (0, 0, 0)
 white = (255, 255, 255)
 
@@ -19,33 +22,37 @@ def sleep(seconds):
 # program 50 lights with the default brightness 1.0, and autoWrite true
 #pixels = neopixel.NeoPixel(board.D18, num_lights)
 
-#pixels = neopixel.NeoPixel(board.D18, 250, pixel_order=neopixel.RGBW)
-pixels = neopixel.NeoPixel(board.D18, 250)
+# following did not do what I thought it would
+#pixels = neopixel.NeoPixel(board.D18, num_lights, pixel_order=neopixel.RGBW)
+pixels = neopixel.NeoPixel(board.D18, num_lights)
+
+# set first LED to green
 #pixels[0] = (255, 0, 0, 0)
 
 #pixels.fill(black)
 #print('red')
-#pixels[0] = (255, 0, 0)
+#pixels[0] = t((255, 0, 0))
 #sleep(1)
 #print('green')
-#pixels[0] = (0, 255, 0)
+#pixels[0] = t((0, 255, 0))
 #sleep(1)
 #print('blue')
-#pixels[0] = (0, 0, 255)
+#pixels[0] = t((0, 0, 255))
 #sleep(1)
 #pixels.fill(black)
 
 # light 20 bright green
-#pixels[19] = (0,255,0)
+#pixels[19] = t((0,255,0))
 # light all pixels red
-#pixels.fill((255,0,0))
+#pixels.fill(t((255,0,0)))
 # turn off neopixels
 pixels.fill(black)
 # green, red, blue
 # red, green, blue
 
-# colors must be supplied to pixels as grb colors,
-# but I think in rgb
+# Colors must be supplied to pixels as grb colors,
+# but I think in rgb.
+
 # green, red, blue colors translated from rgb
 black   = t((  0,   0,   0))
 blue    = t((000, 000, 255))
@@ -57,15 +64,17 @@ yellow  = t((255, 255, 000))
 white   = t((255, 255, 255))
 purple  = t((75, 0, 130))
 
-#colors = [
-#    (  0,   0,   0), # black
-#    (  0,   0, 255), # blue
-#    (  0, 255,   0), # green
-#    (  0, 255, 255), # cyan
-#    (255,   0,   0), # red
-#    (255,   0, 255), # magenta
-#    (255, 255,   0), # yellow
-#    (255, 255, 255)] # white
+#pixels.fill(black)
+#print('red')
+#pixels[0] = red
+#sleep(1)
+#print('green')
+#pixels[0] = green
+#sleep(1)
+#print('blue')
+#pixels[0] = blue
+#sleep(1)
+#pixels.fill(black)
 
 colors = [
     white,
@@ -84,6 +93,8 @@ def randomColor(x):
     r = random.randint(0,5)
     #print (f'r is: {r}')
     return colors[r] # "christmas light colors"
+
+    #not reached at the moment
     if (x % 2 == 0):
         return colors[random.randint(0,5)]
     return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
@@ -92,7 +103,7 @@ def randomColor(x):
 #    pixels.fill(randomColor(x))
 #    sleep(.010)
 
-delay = 0.025
+delay = 0.04
 while True:
     for x in range(num_lights+seg_length):
         # print(f'setting light {x} to a color')
@@ -102,7 +113,6 @@ while True:
         if ((x >= num_lights) or (x >= seg_length)):
             # print(f'setting light {x-seg_length} to black')
             pixels[x-seg_length] = black
-        
         sleep(delay)
     
     sleep(1)
