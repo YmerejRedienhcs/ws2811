c36a6a2098702411e4eb89063b4623fb673bf370
Jeremy Schneider <jschneider@esri.com>
2021-03-28 20:32:49 -0400

    use optional command line arg

--- off.py
+++ off.py
@@ -4,8 +4,12 @@ import board
 import neopixel
 import time
 import random
-num_lights = 150
-num_lights = int(sys.argv[1])
-# program 50 lights with the default brightness 1.0, and autoWrite true
+
+if len(sys.argv) > 1:
+    num_lights = int(sys.argv[1])
+else:
+    num_lights = 250
+
+# program the number of lights with the default brightness 1.0, and autoWrite true
 pixels = neopixel.NeoPixel(board.D18, num_lights)
 pixels.fill((0,0,0))
--- on.py
+++ on.py
@@ -4,9 +4,13 @@ import board
 import neopixel
 import time
 import random
-num_lights = 150
-num_lights = int(sys.argv[1])
+
+if len(sys.argv) > 1:
+    num_lights = int(sys.argv[1])
+else:
+    num_lights = 250
+
 white = (255,255,255)
-# program 50 lights with the default brightness 1.0, and autoWrite true
+# program the number of  lights with the default brightness 1.0, and autoWrite true
 pixels = neopixel.NeoPixel(board.D18, num_lights)
 pixels.fill((255,255,255))
